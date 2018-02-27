d0f9f511cd0e98fa0a1d0f1faac7c70452d36388
GV <GV@DESKTOP-CBANO9S>
2018-02-27 21:39:31 +0530

    pjt create

---
+++ src/main/java/com/nuevo/NuevoApplication.java
@@ -0,0 +1,12 @@
+package com.nuevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuevoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nuevo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nuevo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NuevoApplication.class);
+	}
+
+}
